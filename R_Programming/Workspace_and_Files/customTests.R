d1b732a911ce20fb57b741a30c778cc3d38ac914
Recordpro <nick.boltin@gmail.com>
2019-01-13 15:51:00 -0500

    Update customTests.R
    new google form

--- R_Programming/Workspace_and_Files/customTests.R
+++ R_Programming/Workspace_and_Files/customTests.R
@@ -26,7 +26,7 @@ getLog <- function(){
 submit_log <- function(){
   
   # Please edit the link below
-  pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSfz0GuokgoDC7SUj9rGQucmJkutzb6pfdfRJQxqB3jkJvG7fw/viewform?usp=pp_url"
+  pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSeJaK9cq-HITGl70kiA7dInJAfg_FDzWmMYgTGNuzZ2WdwLMQ/viewform?usp=pp_url"
   
   # Do not edit the code below
   if(!grepl("=$", pre_fill_link)){
