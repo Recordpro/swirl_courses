a7d1bd1ab7d7fb120f64245c14b330c966f804c7
Recordpro <nick.boltin@gmail.com>
2019-01-13 14:09:00 -0500

    Update customTests.R
    try another link

--- R_Programming/Basic_Building_Blocks/customTests.R
+++ R_Programming/Basic_Building_Blocks/customTests.R
@@ -26,7 +26,7 @@ getLog <- function(){
 submit_log <- function(){
   
   # Please edit the link below
-  pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSc132bnxpF-fSV6MGy-a1JLXLkdLOef_SRNXXa5xibIGO8AjA/viewform?usp=pp_url"
+  pre_fill_link <- "https://docs.google.com/forms/d/1CCIT8GRNQ_wOjVoSfNk7XaM4oCmsi0bbLOoWFrBeztI/prefill"
   
   # Do not edit the code below
   if(!grepl("=$", pre_fill_link)){
